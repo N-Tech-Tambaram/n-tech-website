b16eaf19273ee00b80a26778d9bbee7aa1709e73
NTech <ntechtambaram@gmail.com>
2025-11-24 17:00:58 +0530

    Added new courses: MERN, Cloud & DevOps, Data Science

--- src/App/Pages/Course/Data/data.js
+++ src/App/Pages/Course/Data/data.js
@@ -20,9 +20,10 @@ const CourseDetails = [
   },
   {
     id: 2,
-    courseTitle: "Cloud Computing & DevOps",
+    courseTitle: "Cloud & DevOps",
     content:
       "This course provides hands-on knowledge of cloud platforms and DevOps practices for modern software development.",
+    bgImage: "/logo192.png",
     popupContents: {
       mainContent:
         "You will learn cloud fundamentals, working with AWS, Azure, or Google Cloud, CI/CD pipelines, containerization with Docker, and automation using DevOps tools.",
@@ -38,9 +39,10 @@ const CourseDetails = [
   },
   {
     id: 3,
-    courseTitle: "Data Science & Machine Learning",
+    courseTitle: "Data Science & AI",
     content:
       "This course provides a comprehensive introduction to data science, data analysis, and machine learning using Python.",
+    bgImage: "/logo192.png",
     popupContents: {
       mainContent:
         "You will learn data collection, cleaning, analysis, visualization, and building predictive models using libraries such as NumPy, pandas, Matplotlib, and Scikit-learn.",
@@ -59,6 +61,7 @@ const CourseDetails = [
     courseTitle: "Data Structures and Algorithms",
     content:
       "This introductory course provides a comprehensive foundation in data structures and algorithms, which are fundamental to computer science and programming.",
+    bgImage: "/logo192.png",
     popupContents: {
       mainContent:
         "A data structure is a way of organising and storing data in a computer's memory so that it can be used efficiently. It defines the relationship between data and the operations that can be performed on the data. Common data structures include arrays, linked lists, stacks, queues, trees, and graphs. An algorithm is a step-by-step procedure or set of rules for solving a specific problem or accomplishing a particular task. Algorithms are independent of programming languages and are essential for writing efficient and effective computer programs.",
@@ -77,6 +80,7 @@ const CourseDetails = [
     courseTitle: "C and C++",
     content:
       "This introductory course provides a solid foundation in the C programming language, covering basic concepts, syntax, and programming techniques.",
+    bgImage: "/logo192.png",
     popupContents: {
       mainContent:
         "C is a general-purpose programming language that was created by Dennis Ritchie at Bell Labs in the early 1970s. C++ is an extension of C with additional features, combining procedural and object-oriented programming.",
@@ -95,6 +99,7 @@ const CourseDetails = [
     courseTitle: "Python MySQL Package",
     content:
       "This comprehensive course is designed to equip students with the knowledge and skills to develop Python applications that seamlessly interact with MySQL databases.",
+    bgImage: "/logo192.png",
     popupContents: {
       mainContent:
         "Python programming is a versatile and popular programming language that is widely used for data exploration and analysis. Python provides powerful libraries such as NumPy and pandas for data manipulation and analysis.",
@@ -113,6 +118,7 @@ const CourseDetails = [
     courseTitle: "Java MySQL Package",
     content:
       "This course provides a solid foundation in both Java programming and MySQL database management.",
+    bgImage: "/logo192.png",
     popupContents: {
       mainContent:
         "Java is a versatile programming language widely used for developing software, including web applications. MySQL is a popular relational database. You can connect Java apps to MySQL using JDBC.",
@@ -131,6 +137,7 @@ const CourseDetails = [
     courseTitle: "Java React Fullstack",
     content:
       "A Java React Fullstack course typically covers both the Java programming language for backend development and the React JavaScript library for frontend development.",
+    bgImage: "/logo192.png",
     popupContents: {
       mainContent:
         "Java is a high-level, object-oriented programming language. React is a JavaScript library for building dynamic UIs. Fullstack development combines frontend and backend development skills.",
@@ -149,6 +156,7 @@ const CourseDetails = [
     courseTitle: "UI / UX",
     content:
       "This course provides a comprehensive introduction to the principles of user interface and user experience design.",
+    bgImage: "/logo192.png",
     popupContents: {
       mainContent:
         "UI - User Interface refers to the point of interaction between a user and software. UX - User Experience refers to the overall experience that a user has when interacting with a product.",
@@ -167,6 +175,7 @@ const CourseDetails = [
     courseTitle: ".NET (MVC)",
     content:
       "This beginner-level course provides a comprehensive introduction to the .NET framework, including its history and components.",
+    bgImage: "/logo192.png",
     popupContents: {
       mainContent:
         "NET MVC (Model-View-Controller) is a framework for building web applications by separating data, UI, and control logic.",
@@ -185,6 +194,7 @@ const CourseDetails = [
     courseTitle: "Web Designing",
     content:
       "This introductory course covers the fundamental principles of web design including layout, typography, color theory, and UI design.",
+    bgImage: "/logo192.png",
     popupContents: {
       mainContent:
         "Web designing is planning, conceptualizing, and implementing websites for functional and good user experience.",
@@ -201,6 +211,7 @@ const CourseDetails = [
   {
     id: 12,
     courseTitle: "Ms Office",
+    bgImage: "/logo192.png",
     content:
       "This comprehensive Microsoft Word course is designed to help you become proficient in creating, formatting, and editing documents.",
     popupContents: {
@@ -221,6 +232,7 @@ const CourseDetails = [
     courseTitle: "Tally ERP 9 including GST",
     content:
       "Learn accounting, finance, inventory, sales, and purchase management using Tally ERP 9.",
+    bgImage: "/logo192.png",
     popupContents: {
       mainContent:
         "Tally ERP 9 helps business owners manage accounts, billing, taxation, and more with simplicity.",
@@ -239,6 +251,7 @@ const CourseDetails = [
     courseTitle: "Data Analysis and Advanced Microsoft Excel",
     content:
       "This course provides foundational understanding of data analysis techniques using Excel.",
+    bgImage: "/logo192.png",
     popupContents: {
       mainContent:
         "Data analysis includes inspecting, cleansing, transforming, and modeling data to discover useful information and support decision-making.",
@@ -257,6 +270,7 @@ const CourseDetails = [
     courseTitle: "Data Analysis and Visualisation in Power Bi",
     content:
       "This course introduces Power BI for data analysis and visualization.",
+    bgImage: "/logo192.png",
     popupContents: {
       mainContent:
         "Power BI is a business analytics service that lets you visualize your data and share insights through dashboards and reports.",
@@ -275,6 +289,7 @@ const CourseDetails = [
     courseTitle: "Python Programming and Data Exploration",
     content:
       "This course serves as a comprehensive introduction to Python for data science and software development.",
+    bgImage: "/logo192.png",
     popupContents: {
       mainContent:
         "Data Structures and Algorithms form the backbone of efficient programs. Key concepts include arrays, linked lists, stacks, and queues.",
@@ -293,6 +308,7 @@ const CourseDetails = [
     courseTitle: "Social Media Marketing",
     content:
       "This course provides understanding of social media marketing strategies and techniques.",
+    bgImage: "/logo192.png",
     popupContents: {
       mainContent:
         "Social media marketing uses platforms like Facebook, Twitter, and Instagram to build a brand, increase sales, and drive website traffic.",
@@ -311,6 +327,7 @@ const CourseDetails = [
     courseTitle: "SEO and SEM",
     content:
       "This course provides foundation in strategies for optimizing websites for search engines and paid ads.",
+    bgImage: "/logo192.png",
     popupContents: {
       mainContent:
         "SEO improves website visibility in search results. SEM promotes websites through paid advertising on search engines.",
@@ -329,6 +346,7 @@ const CourseDetails = [
     courseTitle: "E-Mail Marketing",
     content:
       "This course introduces email marketing, list building, campaign creation, and performance tracking.",
+    bgImage: "/logo192.png",
     popupContents: {
       mainContent:
         "Email marketing uses emails to promote products or services, engage customers, and generate leads.",
